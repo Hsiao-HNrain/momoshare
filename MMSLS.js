ffc684aca5556d8f357098880116669646b35829
Hsiao-HNrain <85089773+Hsiao-HNrain@users.noreply.github.com>
2023-07-02 20:18:20 +0800

    Update MMSLS.js

--- MMSLS.js
+++ MMSLS.js
@@ -20,7 +20,7 @@ const MMSLS = [
  */
 const MMSLS = [
     
-    "https://www.maimemo.com/share/page?uid=15173471&pid=d9229cbc0be3397b1de4cd060287cc05&tid=44df1a6ed1b92de8ca76463e8adc9190",
+    "https://www.maimemo.com/share/page?uid=15173471&pid=cb6ea4e2c16a6e8780ff7d0a54d6c0c6&tid=7c478926cea0c2f6624951112cdbe2ae",
     "",
     "",
   ]
