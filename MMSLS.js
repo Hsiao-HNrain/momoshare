479e46f7b688ef1740f9185611d4c84564acaee5
Hsiao-HNrain <85089773+Hsiao-HNrain@users.noreply.github.com>
2023-10-17 21:39:58 +0800

    Update MMSLS.js

--- MMSLS.js
+++ MMSLS.js
@@ -20,7 +20,7 @@ const MMSLS = [
  */
 const MMSLS = [
     
-    "https://www.maimemo.com/share/page?uid=15173471&pid=6336fb28cbf861f4b8116c4c567fe128&tid=a4659b06e30f8f94f5fe01e2a45eabe0",
+    "https://www.maimemo.com/share/page?uid=15173471&pid=29e47592f1b4543b3f79569a12264bf4&tid=73cb271116f503339717fcbe80572ab7",
   ]
   /**
    * 生成随机数字
